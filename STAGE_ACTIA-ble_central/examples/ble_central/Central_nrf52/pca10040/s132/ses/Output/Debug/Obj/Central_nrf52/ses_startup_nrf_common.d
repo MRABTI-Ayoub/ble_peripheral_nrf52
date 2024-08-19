cae8a295566ff31a18e482e7b79a0bfad9e629af
MRABTI Ayoub <mrabtiayoub345@gmail.com>
2024-08-19 01:31:04 -0700

    Ajout du dossier ble

---
+++ STAGE_ACTIA-ble_central/examples/ble_central/Central_nrf52/pca10040/s132/ses/Output/Debug/Obj/Central_nrf52/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/Central_nrf52/ses_startup_nrf_common.o: \
+ C\:\Users\MRABTI\ Ayoub\Desktop\STAGE_ACTIA\ -\ Copy\modules\nrfx\mdk\ses_startup_nrf_common.s
---
+++ STAGE_ACTIA-ble_central/examples/ble_central/Central_nrf52/pca10040/s132/ses/Output/Debug/Obj/Central_nrf52/thumb_crt0.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/Central_nrf52/thumb_crt0.o: \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/source/thumb_crt0.s
