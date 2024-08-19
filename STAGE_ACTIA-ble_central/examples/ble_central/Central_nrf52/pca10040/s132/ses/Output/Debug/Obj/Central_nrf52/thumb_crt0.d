d2000f196cf4c9c9e293ee09378bcf831762124d
MRABTI Ayoub <159964924+MRABTI-Ayoub@users.noreply.github.com>
2024-08-19 01:40:11 -0700

    Delete STAGE_ACTIA-ble_central directory

--- STAGE_ACTIA-ble_central/examples/ble_central/Central_nrf52/pca10040/s132/ses/Output/Debug/Obj/Central_nrf52/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/Central_nrf52/thumb_crt0.o: \
- C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/source/thumb_crt0.s
--- STAGE_ACTIA-ble_central/examples/ble_central/Central_nrf52/pca10040/s132/ses/Output/Debug/Obj/Central_nrf52/utf.d
+++
@@ -1,6 +0,0 @@
-Output/Debug/Obj/Central_nrf52/utf.o: \
- C\:\Users\MRABTI\ Ayoub\Desktop\STAGE_ACTIA\ -\ Copy\external\utf_converter\utf.c \
- C\:\Users\MRABTI\ Ayoub\Desktop\STAGE_ACTIA\ -\ Copy\external\utf_converter\utf.h \
- C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stddef.h \
- C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/__crossworks.h \
- C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stdint.h
